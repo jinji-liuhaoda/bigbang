e90e1a480e9000cc9a778773817dc52d320a32dd
jinji-liuhai <liuhai@jinji-inc.com>
2016-05-31 15:40:45 +0800

    微信支付bug修改

--- ucenter/urls.py
+++ ucenter/urls.py
@@ -7,11 +7,13 @@ from ucenter import auth_views, wx_auth, wx_pay
 urlpatterns = [
     # 用户中心
     url(r'^login/$', auth_views.login, name='login'),
+    url(r'^retrieve/$', auth_views.retrieve, name='retrieve'),
     url(r'^login_out/$', auth_views.login_out, name='login_out'),
     url(r'^register/$', auth_views.register, name='register'),
     url(r'^register_do/$', auth_views.register_do, name='register_do'),
     url(r'^index$', auth_views.index, name='index'),
     url(r'^pwd_update/$', auth_views.pwd_update, name='pwd_update'),
+    url(r'^retrieve_pwd/$', auth_views.retrieve_pwd, name='retrieve_pwd'),
     url(r'^phone/(?P<phone>\d+)$', auth_views.check_phone, name='check_phone'),
     url(r'^send_code/(?P<phone>\d+)$', auth_views.send_code, name='send_code'),
     # 微信相关
